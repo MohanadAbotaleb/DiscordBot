7b9205534100f9605f6e63e526a5813cb587de9f
Mohanad31 <mohanado2370@gmail.com>
2021-03-11 13:24:23 +0200

    basic functionality of the bot

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const Discord = require('discord.js');
 require('dotenv').config();
-const client = new Discord.Client();
 
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('bot is ready!');
+  });
+  
+  const prefix = "g!";
+  client.on("message", (message) => {
+    if(!msg.content.startsWith(prefix) || message.author.bot) return;
+    if (message.content.startsWith(prefix + "ping")) {
+      message.channel.send("pong!");
+    }
+  });
+
+
+
+
+  
 client.login(process.env.TOKEN);
